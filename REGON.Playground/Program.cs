bf7f2c77c27d6f82431e7ea511becb0410913137
Jarosław Armatys <jarekarmatys@gmail.com>
2026-01-13 12:09:09 +0100

    Dodanie obsługi NIP oraz wyświetlanie nazw firm w programie

--- REGON.Playground/Program.cs
+++ REGON.Playground/Program.cs
@@ -6,7 +6,7 @@ namespace REGON.Playground
 {
     public abstract class Program
     {
-        private const string GusKey = "<YOUR-GUS-KEY>";
+        private const string GusKey = "b74d739805a34c7d837d";
         
         private static async Task Main(string[] args)
         {
@@ -16,8 +16,16 @@ namespace REGON.Playground
             
             const string exampleKrs = "0000709622";
             
-            var regonResponse = await client.GetCompanyDataByKrs(exampleKrs);            
+            var krsResponse = await client.GetCompanyDataByKrs(exampleKrs);
 
+            Console.WriteLine(krsResponse.Name);
+
+            const string exampleNip = "6392021335";
+            
+            var nipResponse = await client.GetCompanyDataByNip(exampleNip);
+
+            Console.WriteLine(nipResponse.Name);
+            
             Console.WriteLine("REGON PLAYGROUND - END");
         }
 
